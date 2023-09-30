894017f989d7a526d604c106106576d17a8ccbee
Laura hp <laura.fabbiano.gomez@hotmail.com>
2023-09-30 21:51:40 +0200

    testing functions and main file

---
+++ Re-Learning_Python/functions.py
@@ -0,0 +1,9 @@
+def test_function(x) :
+    if x >= 1 :
+        print("it's a positive number")
+    else :
+        print("it's a negative number")
+    return x
+
+def test_function2(message) :
+    print(message)
---
+++ Re-Learning_Python/main.py
@@ -0,0 +1,4 @@
+import functions
+
+result = functions.test_function(5)
+functions.test_function2("hello")
